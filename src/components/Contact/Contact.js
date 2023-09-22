45a00034bcd6c9fa37efb37c7e6576597ae415c7
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-09-22 10:03:36 +0200

    mailto corrections

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,26 +6,37 @@ import "./Contact.css"; // Adjust the import path for your CSS file
 function Contact() {
   return (
     <div>
-      <Element className="contact" id="contact">
-        <h1>Contact</h1>
-        <div className="contact__container">
-          <p>
-            Email: <span>rampriya.karthick@gmail.com</span>
-          </p>
-          <p>
-            GitHub Username: <span>RampriyaKarthick</span>
-          </p>
-          <div className="contact__icons">
-            <a href="https://github.com/RampriyaKarthick">
-              <GitHub />
-            </a>
-            <a href="https://www.linkedin.com/in/rampriya-karthick/">
-              <LinkedIn />
-            </a>
-          </div>
-        </div>
-      </Element>
+  <Element className="contact" id="contact">
+    <h1>Contact</h1>
+    <div className="contact__container">
+    <p>
+    Email: <span>
+     rampriya.karthick@gmail.com
+    </span>
+   
+   </p>
+   <p>
+    or
+   </p>
+  <p>
+  Click to email directly: <span><a href="mailto:rampriya.karthick@gmail.com"><span>rampriya.karthick@gmail.com</span></a></span>
+  </p>
+
+      <p>
+        GitHub Username: <span>RampriyaKarthick</span>
+      </p>
+      <div className="contact__icons">
+        <a href="https://github.com/RampriyaKarthick">
+          <GitHub />
+        </a>
+        <a href="https://www.linkedin.com/in/rampriya-karthick/">
+          <LinkedIn />
+        </a>
+      </div>
     </div>
+  </Element>
+</div>
+
   );
 }
 
