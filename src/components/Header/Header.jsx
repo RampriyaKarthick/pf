f0d962a8b56164a4216642e87b70d8329091f672
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-02-21 09:09:36 +0100

    hamburger nav for small devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import {Link} from "react-scroll"
-import "./Header.css"
-
+import React, { useState, useEffect} from 'react';
+import { Link } from 'react-scroll';
+import './Header.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('isMenuOpen', isMenuOpen);
+  }, [isMenuOpen]);
+
   return (
     <div className="header">
-<div className="header__left">
-    <h1>Develop<span>er</span></h1>
-</div>
-<div className="header__right">
-<Link to="about" smooth={true} duration={500}>
-<h4> About me</h4>
-</Link>
-<Link to="skills" smooth={true} duration={500}>
+      <div className="header__left">
+        <h1>
+           Develop<span>er</span>
+        </h1>
+      </div>
+      <div className="header__right">
+        <div className="menu" onClick={toggleMenu}>
+          <div className={`menu-line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`menu-line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`menu-line ${isMenuOpen ? 'open' : ''}`}></div>
+        </div>
+        {isMenuOpen && (
+          <div className="nav">
+            <Link to="about" smooth={true} duration={500}>
+              <h4>About me</h4>
+            </Link>
+            <Link to="skills" smooth={true} duration={500}>
 <h4> Skills</h4>
 </Link>
 <Link to="project" smooth={true} duration={500}>
@@ -25,12 +43,11 @@ function Header() {
 <Link to="contact" smooth={true} duration={500}>
 <h4>Contacts</h4>
 </Link>
-{/* <h4 className="header__rightButton">
-    Join with me
-</h4> */}
-</div>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
